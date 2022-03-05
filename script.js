e79fed5c504b0eed5eb83c3e9d3d05c26241fcd5
Chetanna E <che@Chetannas-MacBook-Pro.local>
2022-03-05 14:31:13 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+let weather = {
+    apiKey : "25bf734a39bdfaf022a4f3220765b301",
+    fetchWeather: function(city){
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            +city
+            + "&units=imperial&appid=" 
+            + this.apiKey
+        ).then((response) => response.json()).then((data)=>this.displayWeather(data));
+    },
+    displayWeather: function(data){
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed} = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon +".png"
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + "m/h";
+        document.querySelector(".weather").classList.remove("loading");
+       
+    },
+    search: function (){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+
+document
+    .querySelector(".search button")
+    .addEventListener("click", function(){
+        weather.search();
+    });
+document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    });
+weather.fetchWeather("Tokyo");
